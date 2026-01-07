100664314c6149941cfd2f35e8e6924a290f97f3
Sharaf E. Boukhezer <sharaf2907@gmail.com>
2026-01-08 00:25:49 +0100

    Time: 0 ms (100.00%) | Memory: 12.4 MB (60.95%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+
+        lcp = strs[0]
+
+        for s in strs[1:]:
+            while not s.startswith(lcp):
+                lcp = lcp[:-1]
+            if lcp == "":
+                return ""
+
+        return lcp
+        
